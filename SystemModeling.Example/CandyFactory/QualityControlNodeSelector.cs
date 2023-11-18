4fde736560a6e5fa24801f696950bcebb6800bc9
Mykyta Kovalenko <nikkv.ottf@gmail.com>
2023-11-18 18:59:52 +0100

    adjust quality control node selector

--- SystemModeling.Example/CandyFactory/QualityControlNodeSelector.cs
+++ SystemModeling.Example/CandyFactory/QualityControlNodeSelector.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using SystemModeling.Example.CandyFactory.Candies;
+using SystemModeling.Example.Extensions;
 using SystemModeling.Network;
 using SystemModeling.Network.Selectors;
 
@@ -8,12 +8,35 @@ namespace SystemModeling.Example.CandyFactory;
 
 internal sealed class QualityControlNodeSelector(DisposeNode<ICandy> finishedCandy, DisposeNode<ICandy> defectiveCandy) : INetworkNodeSelector<ICandy>
 {
-    private readonly Dictionary<Type, INetworkNodeSelector<ICandy>> _nodeSelectors = new()
+    public NetworkNode<ICandy> GetNext(ref ICandy item) => item switch
     {
-        { typeof(ChocolateCandy), new WeightedNodeSelector<ICandy>((finishedCandy, 0.95f), (defectiveCandy, 0.05f)) },
-        { typeof(CaramelCandy), new WeightedNodeSelector<ICandy>((finishedCandy, 0.96f), (defectiveCandy, 0.04f)) },
-        { typeof(GummyCandy), new WeightedNodeSelector<ICandy>((finishedCandy, 0.99f), (defectiveCandy, 0.01f)) }
+        ChocolateCandy x => GetNodeForChocolateCandy(x),
+        CaramelCandy x => GetNodeForCaramelCandy(x),
+        GummyCandy x => GetNodeForGummyCandy(x),
+        _ => throw new NotSupportedException()
     };
 
-    public NetworkNode<ICandy> GetNext(ref ICandy item) => _nodeSelectors[item.GetType()].GetNext(ref item);
+    private NetworkNode<ICandy> GetNodeForChocolateCandy(ChocolateCandy candy)
+    {
+        float threshold = 0.05f + 0.01f * ((int)candy.Additives).SetBitsCount();
+        return Random.Shared.NextSingle() <= threshold ? defectiveCandy : finishedCandy;
+    }
+
+    private NetworkNode<ICandy> GetNodeForCaramelCandy(CaramelCandy candy)
+    {
+        float threshold = candy.Aroma switch
+        {
+            CaramelAroma.Strawberry => 0.04f,
+            CaramelAroma.Apricot => 0.045f,
+            _ => throw new NotSupportedException()
+        };
+
+        return Random.Shared.NextSingle() <= threshold ? defectiveCandy : finishedCandy;
+    }
+
+    private NetworkNode<ICandy> GetNodeForGummyCandy(GummyCandy candy)
+    {
+        float threshold = 0.05f + 0.01f * ((int)candy.Additives).SetBitsCount();
+        return Random.Shared.NextSingle() <= threshold ? defectiveCandy : finishedCandy;
+    }
 }
